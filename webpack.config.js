59064a055bd787494fc25bbdf7c0b16d34d71ef8
Blackbaud-AlexFortin <alex.fortin@blackbaud.com>
2015-12-22 16:05:52 -0600

    bleh

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/app/public/'
+    publicPath: './public/'
   },
   plugins: [
     new webpack.optimize.DedupePlugin(),
