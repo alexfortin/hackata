54e6e483fd06b0bbf9c02771fe3075aff7c5c2c6
Blackbaud-AlexFortin <alex.fortin@blackbaud.com>
2015-12-22 15:31:12 -0600

    changed port

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var express = require('express');
+var app = express();
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true
-}).listen(3000, 'localhost', function (err, result) {
-  if (err) { console.log(err) }
-  console.log('Listening at localhost:3000');
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.render('index');
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
 });
