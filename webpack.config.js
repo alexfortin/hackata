013b5938149ed42e9a2c9ccb3e8b165e6a9587c6
Blackbaud-AlexFortin <alex.fortin@blackbaud.com>
2015-12-22 13:01:36 -0600

    Restructure

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,46 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
-  entry: "./app/components/Main.js",
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/Main'
+  ],
+
   output: {
-    filename: "public/bundle.js"
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
   },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+
   module: {
     loaders: [
-      {
-        test: /\.jsx?$/,
+      { test: /\.jsx$/,
+        loader: 'react-hot!babel',
+        include: path.join(__dirname, 'src') },
+      { test: /\.js?$/,
         exclude: /(node_modules)/,
         loader: 'babel',
         query: {
          cacheDirectory: true,
-         presets: ['react', 'es2015']
-       }
-     },
-     {
-       test: /\.(jpe|jpg|png|woff2|eot|ttf|svg)(\?.*$|$)/,
-       loader: 'url-loader?limit=100000'
-     },
-     {
-      test: /\.css$/, // Only .css files
-      loader: 'style!css' // Run both loaders
-    }
+         presets: ['react', 'es2015'] }},
+       { test: /\.(jpe|jpg|png|woff2|eot|ttf|svg)(\?.*$|$)/,
+         loader: 'url-loader?limit=100000' },
+       { test: /\.scss?$/,
+         loader: 'style!css!sass',
+         include: path.join(__dirname, 'css') },
+       { test: /\.css$/,
+         loader: 'style!css' }
     ]
   }
 };
