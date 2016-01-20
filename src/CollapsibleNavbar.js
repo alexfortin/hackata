90045335e42696fe18eed0a3e7495065c3063717
Blackbaud-AlexFortin <alex.fortin@blackbaud.com>
2016-01-20 14:43:49 -0600

    Mobile mode

---
+++ src/CollapsibleNavbar.js
@@ -0,0 +1,39 @@
+var React = require('react');
+var Navbar = require("react-bootstrap/lib/Navbar");
+var Nav = require("react-bootstrap/lib/Nav");
+var NavItem = require("react-bootstrap/lib/NavItem");
+var NavDropdown = require("react-bootstrap/lib/NavDropdown");
+var MenuItem = require("react-bootstrap/lib/MenuItem");
+
+module.exports= React.createClass({
+
+    getInitialState: function() {
+        return {expanded: false};
+    },
+    onNavItemClick: function() {
+        this.setState({ expanded: false });
+
+    },
+    onNavbarToggle: function() {
+        this.setState({ expanded: ! this.state.expanded });
+    },
+
+    render: function (){
+        return (
+            <Navbar fixedTop className="navbar-border" expanded={this.state.expanded}onToggle={ this.onNavbarToggle } >
+                <Navbar.Header>
+                    Hakata Sushi
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <MenuItem onClick={ this.onNavItemClick } eventKey={1} href="#menu-tab">MENU</MenuItem>
+                        <MenuItem onClick={ this.onNavItemClick } eventKey={2} href="#location">LOCATION AND HOURS</MenuItem>
+                        <MenuItem onClick={ this.onNavItemClick } eventKey={2} href="#contact-info">CONTACT</MenuItem>
+                        <MenuItem onClick={ this.onNavItemClick } eventKey={2} href="#about-info">ABOUT</MenuItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+});
--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,19 @@
 import '../public/main.css';
 
 var React = require('react'),
- Sticky = require('react-sticky');
+    Sticky = require('react-sticky');
 var ReactDom = require('react-dom');
 var About = require('./About');
-var Navbar = require('./Navbar');
+var Navbar2 = require('./Navbar');
 var Greeting = require('./Greeting');
 var Menu = require('./Menu');
 var LocationAndContact = require('./LocationAndContact');
+var Navbar = require("react-bootstrap/lib/Navbar");
+var Nav = require("react-bootstrap/lib/Nav");
+var NavItem = require("react-bootstrap/lib/NavItem");
+var NavDropdown = require("react-bootstrap/lib/NavDropdown");
+var MenuItem = require("react-bootstrap/lib/MenuItem");
+var NavBarTest = require('./CollapsibleNavbar');
 
 module.exports= React.createClass({
   render: function (){
@@ -24,9 +30,25 @@ module.exports= React.createClass({
   }
 });
 
+const navbarInstance = (
+    <Navbar fixedTop toggleNavKey={0} className="navbar-border">
+        <Navbar.Header>
+            Hakata Sushi
+            <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+            <Nav>
+                <MenuItem eventKey={1} href="#misc-info">Location</MenuItem>
+                <MenuItem eventKey={2} href="#">Link Right</MenuItem>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+);
 
-ReactDom.render(<Greeting />, document.getElementById('greeting'));
-ReactDom.render(<Navbar />, document.getElementById('navbar'));
+
+
+ReactDom.render(<Navbar2 />, document.getElementById('navbar'));
+ReactDom.render(<NavBarTest/>, document.getElementById('navbar2'));
 ReactDom.render(<Menu />, document.getElementById('tab'));
 ReactDom.render(<LocationAndContact />, document.getElementById('misc-info'));
 ReactDom.render(<About />, document.getElementById('about-info'));
