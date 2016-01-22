ba855189ec7f2680b7fd08a86924f420c0bcc84d
Blackbaud-AlexFortin <alex.fortin@blackbaud.com>
2016-01-22 11:39:25 -0600

    More cleanup

--- src/Main.js
+++ src/Main.js
@@ -1,22 +1,18 @@
 import '../public/main.css';
 
-var React = require('react'),
-    Sticky = require('react-sticky');
+var React = require('react');
 var ReactDom = require('react-dom');
 var About = require('./About');
-var Navbar2 = require('./Navbar');
+var Navbar = require('./Navbar');
 var Greeting = require('./Greeting');
 var Menu = require('./Menu');
+var Greeting = require('./Greeting');
 var LocationAndContact = require('./LocationAndContact');
-var Navbar = require("react-bootstrap/lib/Navbar");
-var Nav = require("react-bootstrap/lib/Nav");
-var NavItem = require("react-bootstrap/lib/NavItem");
-var NavDropdown = require("react-bootstrap/lib/NavDropdown");
-var MenuItem = require("react-bootstrap/lib/MenuItem");
-var NavBarTest = require('./CollapsibleNavbar');
+var CollapsibleNavbar = require('./CollapsibleNavbar');
 
-ReactDom.render(<Navbar2 />, document.getElementById('navbar'));
-ReactDom.render(<NavBarTest/>, document.getElementById('navbar2'));
+ReactDom.render(<Navbar />, document.getElementById('navbar'));
+ReactDom.render(<CollapsibleNavbar/>, document.getElementById('CollapsibleNavbar'));
+ReactDom.render(<Greeting/>, document.getElementById('greeting'));
 ReactDom.render(<Menu />, document.getElementById('tab'));
 ReactDom.render(<LocationAndContact />, document.getElementById('misc-info'));
 ReactDom.render(<About />, document.getElementById('about-info'));
